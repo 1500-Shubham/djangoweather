ef13f0666fe65f1d1f935b48af28d1514a607bd0
Shubham <shubham@gmail.com>
2022-07-18 13:24:56 +0530

    Project-1-Weather App Done

--- lookup/views.py
+++ lookup/views.py
@@ -3,6 +3,20 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request): #pyhton templates connect
-	return render(request,'home.html',{})
+	import json
+	import requests #out of internet jayega
+
+	api_request=requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=text/csv&zipCode=89129&distance=5&API_KEY=F30172B7-CBF0-432D-A320-7BB5F5487E48")
+	#python things data se`
+	try:
+		api=json.loads(api_request.content)
+		#try to get data is some error niche gaya
+		#aqi is array of dictionary
+
+	except Exception as e:
+		api="Error...."
+
+	return render(request,'home.html',{'api' : api})
+
 def about(request): #pyhton templates connect
 	return render(request,'about.html',{})
